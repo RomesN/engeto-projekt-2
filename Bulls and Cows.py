849be0b7626d886fc2f5f4aa4ba66574dc4a02ca
Roman Németh <romannemeth1@gmail.com>
2022-01-16 00:52:08 +0100

    First working game

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -19,18 +19,15 @@ while secret_number[0] == 0:
 # Guessing
 guess = int()
 guess_list = []
+clean_guess_counter = 0
+wrong_input_counter = 0
 while not guess_list:
     try:
-        guess = int(input())
+        guess = int(guess_txt := input())
         wrong_input = False
         bulls = 0
         cows = 0
         for i, n in enumerate(str(guess)):
-            # Checking correct lenght
-            if guess not in range(1000, 10000):
-                print("The number must have 4 digits! Try again...")
-                guess_list = []
-                break
             guess_list.append(int(n))
             # Checking for repeating numbers
             for idx in range(0, i):
@@ -39,11 +36,19 @@ while not guess_list:
                     wrong_input = True
                     break
             if wrong_input:
+                wrong_input_counter += 1
                 guess_list = []
                 break
             # Checking for 0 at the start
-            if guess_list[0] == 0:
+            if guess_txt[0] == "0":
                 print("Entered number cannot start with 0! Try again...")
+                wrong_input_counter += 1
+                guess_list = []
+                break
+            # Checking correct length
+            if guess not in range(1000, 10000):
+                print("The number must have 4 digits! Try again...")
+                wrong_input_counter += 1
                 guess_list = []
                 break
             # Checking whether the number is bull or a cow
@@ -54,12 +59,17 @@ while not guess_list:
         else:
             # Victory case
             if bulls == 4:
+                clean_guess_counter += 1
                 print("Correct, you've guessed the right number!")
+                print(f"It took you {clean_guess_counter} guesses "
+                      f"and {wrong_input_counter} wrong inputs.")
             # Informing about bulls and cows
             else:
+                clean_guess_counter += 1
                 print(f"{bulls} bull", end=",") if bulls == 1 else print(f"{bulls} bulls", end=",")
                 print(f"{cows} cow") if cows == 1 else print(f"{cows} cows")
                 guess_list = []
     # Case if non positive number or non-numeric type entered
     except ValueError:
+        wrong_input_counter += 1
         print("Entered value must be a non negative number! Try again...")
