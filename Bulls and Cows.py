f0fe73e0b8c01dbe86cebdd8b84be0723a581d05
Roman Németh <romannemeth1@gmail.com>
2022-01-21 20:15:18 +0100

    Change of solution method

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -2,11 +2,11 @@ from random import sample
 from time import time
 
 
-# Evaluation def
-def overall_eval(clean_counter, t_time) -> str:
+# Evaluation
+def overall_eval(clean_counter: int, t_time: float) -> str:
     if clean_counter < 5 or t_time < 20:
         result = "amazing"
-    elif clean_counter < 7 and t_time < 50:
+    elif clean_counter < 7 and 50 > t_time:
         result = "very good"
     elif clean_counter < 10 and t_time < 60:
         result = "good"
@@ -18,92 +18,115 @@ def overall_eval(clean_counter, t_time) -> str:
 
 
 # Greeting
-separator = "-"
-print("Hi there!")
-print(separator * 47)
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
-print(separator * 47)
-print("Enter a number 4 digit number:")
-print(separator * 47)
+def greeting() -> None:
+    separator = "-"
+    print("Hi there!")
+    print(separator * 47)
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(separator * 47)
+    print("Enter a number 4 digit number:")
+    print(separator * 47)
+    return None
 
-# NumberToGuess
-secret_source = list(range(0, 10))
-secret_number = [0, 0, 0, 0]
-while secret_number[0] == 0:
-    secret_number = sample(secret_source, 4)
 
-# Guessing
-guess = int()
-guess_list = []
-clean_guess_counter = 0
-wrong_input_counter = 0
-total_time = 0
-start_time = time()
-while not guess_list:
+# Number to guess
+def to_guess() -> list:
+    secret_source = list(range(0, 10))
+    secret_number = [0, 0, 0, 0]
+    while secret_number[0] == 0:
+        secret_number = sample(secret_source, 4)
+    return secret_number
+
+
+# Input check
+def inpt_check(inpt: str) -> bool:
     try:
-        guess = int(guess_txt := input())
-        wrong_input = False
-        bulls = 0
-        cows = 0
-        for i, n in enumerate(str(guess)):
-            guess_list.append(int(n))
-            # Checking for repeating numbers
-            for idx in range(0, i):
-                if guess_list[idx] == guess_list[i]:
-                    print("There cannot be any repeating numbers! Try again...")
-                    wrong_input = True
-                    break
-            if wrong_input:
-                wrong_input_counter += 1
-                guess_list = []
-                break
-            # Checking for 0 at the start
-            if guess_txt[0] == "0":
-                print("Entered number cannot start with 0! Try again...")
-                wrong_input_counter += 1
-                guess_list = []
-                break
-            # Checking correct length
-            if guess not in range(1000, 10000):
-                print("The number must have 4 digits! Try again...")
-                wrong_input_counter += 1
-                guess_list = []
-                break
-            # Checking whether the number is bull or a cow
-            elif guess_list[i] == secret_number[i]:
-                bulls += 1
-            elif guess_list[i] in secret_number:
-                cows += 1
+        int_input = int(inpt)
+        # starting with 0?
+        if inpt[0] == "0":
+            print("Entered number cannot start with 0! Try again...")
+            return False
+        # 4 digits?
+        elif int_input > 9999 or int_input < 1000:
+            print("The number must have 4 digits! Try again...")
+            return False
+        # Duplicates?
+        elif len(list(inpt)) != len(set(list(inpt))):
+            print("There cannot be any repeating numbers! Try again...")
+            return False
         else:
-            # Victory case
-            if bulls == 4:
-                end_time = time()
-                total_time = end_time - start_time
-                clean_guess_counter += 1
-                print("Correct, you've guessed the right number!")
-                if clean_guess_counter == 1:
-                    print(f"It took you {clean_guess_counter} guess", end="")
-                else:
-                    print(f"It took you {clean_guess_counter} guesses", end="")
-                if wrong_input_counter == 1:
-                    print(f", {wrong_input_counter} wrong input.")
-                else:
-                    print(f", {wrong_input_counter} wrong inputs")
-                print("and {:.2f} seconds.".format(total_time))
-                print(separator * 47)
-            # Informing about bulls and cows
-            else:
-                clean_guess_counter += 1
-                print(f"{bulls} bull", end=", ") if bulls == 1 else print(f"{bulls} bulls", end=", ")
-                print(f"{cows} cow") if cows == 1 else print(f"{cows} cows")
-                print(separator * 47)
-                guess_list = []
-    # Case if non positive number or non-numeric type entered
+            return True
+        # Contains non numerical characters?
     except ValueError:
-        wrong_input_counter += 1
         print("Entered value must be a non negative number! Try again...")
+        return False
 
-# Evaluation
-evaluation = overall_eval(clean_guess_counter, total_time)
-print(f"That's {evaluation}.")
+
+# Input check
+def converter(inpt: str) -> list:
+    list_to_guess = list(inpt)
+    for idx in range(0, 4):
+        list_to_guess[idx] = int(list_to_guess[idx])
+    return list_to_guess
+
+
+# Bulls cows calculator
+def bulls_cows(guessed_list: list, to_guess_list: list) -> tuple:
+    bulls = 0
+    cows = 0
+    for idx in range(0, 4):
+        if guessed_list[idx] == to_guess_list[idx]:
+            bulls += 1
+        elif guessed_list[idx] in to_guess_list:
+            cows += 1
+    return bulls, cows
+
+
+def victory_result(clean_guess_counter: int, wrong_input_counter: int, total_time: float) -> None:
+    separator = "-"
+    print("Correct, you've guessed the right number!")
+    if clean_guess_counter == 1:
+        print(f"It took you {clean_guess_counter} guess", end="")
+    else:
+        print(f"It took you {clean_guess_counter} guesses", end="")
+    if wrong_input_counter == 1:
+        print(f", {wrong_input_counter} wrong input.")
+    else:
+        print(f", {wrong_input_counter} wrong inputs")
+    print("and {:.2f} seconds.".format(total_time))
+    print(separator * 47)
+
+
+# Main
+def main() -> None:
+
+    greeting()
+
+    # variable initialization
+    num_to_guess = to_guess()
+    wrong_input_counter = 0
+    bulls = 0
+    clean_guess_counter = 0
+
+    # guessing
+    start_time = time()
+    while bulls != 4:
+        while not inpt_check(guessed_num := input()):
+            wrong_input_counter += 1
+        clean_guess_counter += 1
+        guessed_num_list = converter(guessed_num)
+        bulls, cows = bulls_cows(guessed_num_list, num_to_guess)
+        print(f"{bulls} bull", end=", ") if bulls == 1 else print(f"{bulls} bulls", end=", ")
+        print(f"{cows} cow") if cows == 1 else print(f"{cows} cows")
+    total_time = time() - start_time
+
+    # printing results
+    victory_result(clean_guess_counter, wrong_input_counter, total_time)
+    print(f"That's {overall_eval(clean_guess_counter, total_time)}.")
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
